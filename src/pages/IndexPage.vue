c9ecb8f78b9a165e6ef12aee4f9fba5902aabe12
Cody <cody@bitaccel.com>
2023-03-04 21:30:54 +0100

    add dropdown for socials

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -13,9 +13,17 @@
         <q-item clickable>
           <q-item-label class="text-white poiret-one text-h3 text-center text-shadow">about us</q-item-label>
         </q-item>
-        <q-item clickable>
-          <q-item-label class="text-white poiret-one text-h3 text-center text-shadow">socials</q-item-label>
-        </q-item>
+        <q-expansion-item hide-expand-icon>
+          <template v-slot:header>
+            <q-item-label class="text-white poiret-one text-h3 text-center text-shadow">socials</q-item-label>
+          </template>
+
+          <div class="q-pa-md">
+            <q-btn flat label="Instagram" icon="ion-logo-instagram"/>
+            <q-btn flat label="Facebook" icon="ion-logo-facebook"/>
+            <q-btn flat label="Twitter" icon="ion-logo-twitter"/>
+          </div>
+        </q-expansion-item>
       </q-list>
     </div>
   </q-page>
