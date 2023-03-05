9614ece98e2fa68da180e17984d590bca33d48d9
Cody <cody@bitaccel.com>
2023-03-05 21:57:09 +0100

    add footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,32 @@
 <template>
   <q-layout view="hHh lpr fFf">
-    <q-toolbar class="transparent">
-      <q-space />
-      <div class="column items-center q-py-lg">
-        <h1 class="poiret-one no-margin text-h2 text-white text-center text-shadow">HARLOW<span>'</span>s</h1>
-        <DiamondDivider style="width: 340px;" class="q-pt-xs"/>
-      </div>
-      <q-space />
-    </q-toolbar>
+    <q-header class="transparent">
+      <q-toolbar class="transparent">
+        <q-space />
+        <div class="column items-center q-mt-xl">
+          <h1 class="poiret-one no-margin text-h2 text-white text-center text-shadow">HARLOW<span>'</span>s</h1>
+          <DiamondDivider style="width: 340px;" class="q-pt-xs"/>
+        </div>
+        <q-space />
+      </q-toolbar>
+    </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="transparent">
+      <q-toolbar class="transparent">
+        <q-item dense>
+          <q-item-section>
+            <q-item-label class="poiret-one text-white text-shadow">wednesday – sunday</q-item-label>
+            <q-item-label class="poiret-one text-white text-shadow font-weight-thin">17:00 – 01:00</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-space/>
+        <q-btn round color="transparent" icon="navigation" />
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
@@ -32,5 +47,8 @@ export default defineComponent({
 body {
   background-image: linear-gradient(0deg, rgba(0,0,0,0.75), rgba(0,0,0,0.6)), url('../assets/dope-background.svg');
   background-position: 26.75% 12%;
+}
+.full-width {
+  width: 100%;
 }
 </style>
