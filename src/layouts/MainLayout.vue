de24ec4ee9234a531552c60d84f2fb54d4c138c2
Cody <cody@bitaccel.com>
2023-03-05 16:04:31 +0100

    mobile positioning

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,13 @@
 <template>
   <q-layout view="hHh lpr fFf">
-    <!-- <q-header reveal flat>
-      <q-toolbar></q-toolbar>
-    </q-header> -->
+    <q-toolbar class="transparent">
+      <q-space />
+      <div class="column items-center q-py-lg">
+        <h1 class="poiret-one no-margin text-h2 text-white text-center text-shadow">HARLOW<span>'</span>s</h1>
+        <DiamondDivider style="width: 340px;" class="q-pt-xs"/>
+      </div>
+      <q-space />
+    </q-toolbar>
 
     <q-page-container>
       <router-view />
@@ -12,12 +17,20 @@
 
 <script>
 import { defineComponent } from 'vue'
+import DiamondDivider from '../components/DiamondDivider.vue'
 
 export default defineComponent({
   name: 'MainLayout',
-  components: {},
+  components: { DiamondDivider },
   setup () {
     return {}
   }
 })
 </script>
+
+<style lang="scss">
+body {
+  background-image: linear-gradient(0deg, rgba(0,0,0,0.75), rgba(0,0,0,0.6)), url('../assets/dope-background.svg');
+  background-position: 26.75% 12%;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,7 @@
 <template>
-  <q-page class="cool-background">
-    <div class="column justify-center items-center landing-hero">
-      <h1 class="poiret-one no-margin text-white text-center text-shadow">HARLOW<span>'</span>s</h1>
-      <DiamondDivider style="width: 540px;"/>
-      <q-list dark class="q-mt-md">
+  <q-page>
+    <!-- <div class="column items-center landing-hero">
+       <q-list dark class="q-mt-md">
         <q-item clickable>
           <q-item-label class="text-white poiret-one text-h3 text-center text-shadow">menu</q-item-label>
         </q-item>
@@ -25,12 +23,11 @@
           </div>
         </q-expansion-item>
       </q-list>
-    </div>
+    </div> -->
   </q-page>
 </template>
 
 <script setup>
-import DiamondDivider from '../components/DiamondDivider.vue'
 </script>
 
 <style lang="scss" scoped>
@@ -39,9 +36,6 @@ import DiamondDivider from '../components/DiamondDivider.vue'
 }
 .text-shadow {
   text-shadow: 2px 2px #000;
-}
-.cool-background {
-  background-image: linear-gradient(0deg, rgba(0,0,0,0.75), rgba(0,0,0,0.6)), url('../assets/dope-background.svg');
 }
 .landing-hero {
   height: 1000px;
