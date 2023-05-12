2dbf6aaf9892691fa079dfb31cc575ad3d1dfe44
Cody <cody@bitaccel.com>
2023-05-12 22:09:35 +0200

    gorgeous

--- src/components/CocktailCard.vue
+++ src/components/CocktailCard.vue
@@ -1,29 +1,37 @@
 <template>
   <q-card square>
-    <q-img :src="cocktail.image"></q-img>
-    <q-card-section class="">
-      <div class="text-h4 poiret-one">{{ cocktail.name }}</div>
-      <div class="text-caption">{{ cocktail.description }}</div>
+    <q-img :src="cocktail.image" height="700px"></q-img>
+    <q-card-section class="q-pl-md q-py-md" horizontal>
+      <div class="row">
+        <div class="col-6">
+          <div class="text-h3 poiret-one q-py-lg">{{ cocktail.name }}</div>
+          <div class="text-body1 text-justify">{{ cocktail.description }}</div>
+        </div>
+        <q-separator vertical inset class="q-mx-lg"/>
+        <q-card-section class="col-5 q-py-none">
+          <div class="text-h5 text-center text-weight-thin q-pt-lg">Flavor Profile</div>
+          <FlavorProfileChart v-bind="cocktail"/>
+        </q-card-section>
+      </div>
     </q-card-section>
-    <q-card-actions align="center">
-      <template v-for="(spirit, index) in cocktail.spirits" :key="index">
-        <div class="text-h6 text-subtitle2 text-weight-light q-px-lg">{{ spirit }}</div>
-      </template>
-    </q-card-actions>
   </q-card>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import FlavorProfileChart from './FlavorProfileChart.vue'
 
 export default defineComponent({
   name: 'CocktailCard',
+  components: {
+    FlavorProfileChart
+  },
   props: {
     cocktail: {
       name: String,
       description: String,
-      spirits: Array,
-      image: String
+      image: String,
+      flavorProfile: Array
     }
   },
   setup (props) {
@@ -31,3 +39,6 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page>
     <div class="row justify-center q-mt-xl">
-      <div class="col-12">
+      <div class="col-12 col-md-10">
         <template v-for="cocktail in cocktails" :key="cocktail.name">
           <CocktailCard :cocktail="cocktail"/>
         </template>
@@ -22,20 +22,20 @@ export default defineComponent({
       {
         name: 'Old Fashioned',
         description: 'The Old Fashioned is a classic cocktail that originated in the United States, but has become popular all over the world, including in Spain. It is a simple and elegant drink that has stood the test of time. The Old Fashioned is a rich and complex blend of flavors, with the sweetness of the sugar, the bitterness of the bitters, and the smoky, oaky flavor of the whiskey. The orange or cherry garnish adds a hint of citrus or sweetness, depending on your preference. It\'s a drink that is meant to be savored slowly, allowing you to appreciate the depth and complexity of the flavors.',
-        spirits: ['Whiskey'],
-        image: 'src/assets/cocktails/old-fashioned.jpg'
+        image: 'src/assets/cocktails/old-fashioned.jpg',
+        flavorProfile: [4, 0, 3, 5, 4, 0, 7, 5]
       },
       {
         name: 'Negroni',
         description: 'The Negroni is a classic cocktail that originated in Italy, but has become popular all over the world, including in Spain. It is a bittersweet and refreshing drink. The resulting drink is a vibrant and bold blend of flavors, with the botanical notes of the gin, the bitter and herbal flavors of the Campari, and the sweet and aromatic flavors of the vermouth. The orange garnish adds a hint of citrus, which balances the bitter and sweet flavors. It\'s a drink that is meant to be enjoyed slowly, allowing you to savor the complexity and balance of the flavors.',
-        spirits: ['Gin', 'Campari', 'Vermouth'],
-        image: 'src/assets/cocktails/negroni-maybe.jpg'
+        image: 'src/assets/cocktails/negroni-maybe.jpg',
+        flavorProfile: [4, 5, 10, 0, 0, 10, 6, 5]
       },
       {
         name: 'Man O\' War',
         description: 'The Man O\' War is a powerful and flavorful cocktail that is named after a famous racehorse from 1920s America, who happens to be Seabiscuit\'s grandpa. is a bold and robust blend of flavors, with the rich and smoky notes of the bourbon, the sweet and fruity flavors of the orange curaçao, and the tart and tangy flavors of the lemon juice. The simple syrup adds a touch of sweetness, which balances the sourness of the lemon. The lemon garnish adds a refreshing citrus aroma, which complements the flavors of the drink.',
-        spirits: ['Bourbon', 'Orange Liquor', 'Vermouth'],
-        image: 'src/assets/cocktails/man-o-war.jpg'
+        image: 'src/assets/cocktails/man-o-war.jpg',
+        flavorProfile: [6, 4, 2, 0, 0, 0, 7, 4]
       }
     ]
     return {
