936c0c005d60c51b42a954a58936d4adceb1426b
Cody <cody@bitaccel.com>
2023-05-13 20:41:12 +0200

    turn off multi word comp names

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,6 +69,7 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'vue/multi-word-component-names': 'off',
     
     'prefer-promise-reject-errors': 'off',
 
