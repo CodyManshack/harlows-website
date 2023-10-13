58043bea5d1638d322ef31e16dde1eb8fb71115c
Cody <cody@bitaccel.com>
2023-10-13 18:54:18 +0200

    centralize color classes

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,15 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+.harlows-light-green {
+  background-color: #334D44;
+}
+.harlows-green {
+  background-color: #16342A;
+}
+.harlows-orange {
+  background-color: #D65F27;
+}
+</style>
