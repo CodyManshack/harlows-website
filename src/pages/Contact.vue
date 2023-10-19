af3ff2eefc0556e382ce89961a5c890a90353c82
Cody <cody@bitaccel.com>
2023-10-19 18:07:08 +0200

    improve button layout

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -16,56 +16,30 @@
       <div class="row justify-center items-center text-center q-my-lg">
         <div class="col-md-8 col-sm-12">
           <h2 class="q-mb-sm">{{ t('title') }}</h2>
-          <h3 class="text-subtitle1">{{  t('subtitle') }}</h3>
-          <div class="row justify-center items-center q-gutter-lg q-my-xl">
+          <h3 class="text-subtitle1 q-px-md">{{  t('subtitle') }}</h3>
+          <div class="row justify-center items-center q-gutter-lg q-my-md">
             <q-btn
               square
-              class="col-md-3 col-sm-8 bg-secondary"
+              class="col-md-3 col-8 bg-secondary"
               style="height: 200px;"
-              href="https://maps.app.goo.gl/y8zXASHuxx2pLbyf9"
+              v-for="button in buttons"
+              :key="button.title"
+              :href="button.href"
               target="_blank"
             >
               <div class="row justify-center items-center">
                 <div class="col-12">
-                  <q-icon name="ion-pin" size="lg" color="accent" class="q-py-lg"/>
+                  <q-icon :name="`ion-${button.icon}`" size="lg" color="accent" class="q-py-lg"/>
                 </div>
-                <h5 class="q-my-sm text-weight-medium col-12">{{ t('address') }}</h5>
-                <p class="text-caption">Carrer de Lepant 4,<br />46008 València</p>
-              </div>
-            </q-btn>
-            <q-btn
-              square
-              class="col-md-3 col-sm-8 bg-secondary"
-              style="height: 200px;"
-              href="https://api.whatsapp.com/send?phone=34692940844"
-            >
-              <div class="row justify-center items-center">
-                <div class="col-12">
-                  <q-icon name="ion-logo-whatsapp" size="lg" color="accent" class="q-py-lg"/>
-                </div>
-                <h5 class="q-my-sm text-weight-medium col-12">{{ t('phone') }}</h5>
-                <p class="text-caption">(+34) 692 940 844</p>
-              </div>
-            </q-btn>
-            <q-btn
-              square
-              class="col-md-3 col-sm-8 bg-secondary"
-              style="height: 200px;"
-              href="mailto:info@harlows.bar"
-            >
-              <div class="row justify-center items-center">
-                <div class="col-12">
-                  <q-icon name="ion-mail" size="lg" color="accent" class="q-py-lg"/>
-                </div>
-                <h5 class="q-my-sm text-weight-medium col-12">{{ t('email') }}</h5>
-                <p class="text-caption">info@harlows.bar</p>
+                <h5 class="q-my-sm text-weight-medium col-12">{{ t(button.title) }}</h5>
+                <p class="text-caption col-7 col-md-10">{{ button.subtitle }}</p>
               </div>
             </q-btn>
           </div>
         </div>
       </div>
     </section>
-    <section>
+    <section class="q-pt-xl">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.7956147359905!2d-0.38684602266516505!3d39.47394581247808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd604fe3f94d65cf%3A0x4471acc1b9c7ad98!2sHarlow&#39;s%20Bar!5e0!3m2!1sen!2ses!4v1697395155698!5m2!1sen!2ses"
         width="100%"
@@ -122,7 +96,28 @@ export default defineComponent({
       }
     })
 
-    return { state, v$, images, t }
+    const buttons = [
+      {
+        icon: 'pin',
+        title: 'address',
+        subtitle: 'Carrer de Lepant 4, 46008 València',
+        href: 'https://maps.app.goo.gl/y8zXASHuxx2pLbyf9'
+      },
+      {
+        icon: 'logo-whatsapp',
+        title: 'phone',
+        subtitle: '(+34) 692 940 844',
+        href: 'https://api.whatsapp.com/send?phone=34692940844'
+      },
+      {
+        icon: 'mail',
+        title: 'email',
+        subtitle: 'info@harlows.bar',
+        href: 'mailto:info@harlows.bar'
+      }
+    ]
+
+    return { buttons, state, v$, images, t }
   },
   methods: {}
 })
