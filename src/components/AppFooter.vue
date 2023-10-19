b4b31e355ea50de96ad5b998badd89c2caeeee8e
Cody <cody@bitaccel.com>
2023-10-19 17:31:21 +0200

    rm duplicated info

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,35 +1,29 @@
 <template>
   <q-footer bordered class="bg-accent">
-    <q-toolbar class="transparent q-pa-md no-wrap justify-center">
-      <q-item dense>
-        <q-item-section>
-          <q-item-label>C. Lepant 4, València</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item dense>
-        <q-item-section>
-          <q-item-label>{{ t('daysOpen') }}</q-item-label>
-          <q-item-label>20:00 – 01:30</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-btn flat icon="ion-logo-instagram" href="https://www.instagram.com/harlows.bar/" target="_blank"/>
-      <q-btn flat icon="ion-logo-facebook" href="https://www.facebook.com/people/Harlows-Bar/100093700941605/" target="_blank"/>
-      <q-btn flat icon="ion-mail" href="mailto:kalen@harlows.bar"/>
-      <q-btn flat icon="ion-navigate" href="https://maps.app.goo.gl/y8zXASHuxx2pLbyf9" target="_blank"/>
-      <q-btn flat icon="ion-logo-whatsapp" href="https://api.whatsapp.com/send?phone=34692940844" />
-    </q-toolbar>
+    <div class="row justify-center items-center q-py-md">
+      <div class="col-md-9 col-12 flex items-center justify-center">
+        <q-btn flat icon="ion-logo-instagram" href="https://www.instagram.com/harlows.bar/" target="_blank"/>
+        <q-btn flat icon="ion-logo-facebook" href="https://www.facebook.com/people/Harlows-Bar/100093700941605/" target="_blank"/>
+        <q-btn flat icon="ion-logo-whatsapp" href="https://api.whatsapp.com/send?phone=34692940844" />
+        <q-btn flat icon="ion-mail" href="mailto:kalen@harlows.bar"/>
+        <q-btn flat icon="ion-navigate" href="https://maps.app.goo.gl/y8zXASHuxx2pLbyf9" target="_blank"/>
+      </div>
+    </div>
+    <div class="row justify-center items-center">
+      <div class="col-md-9 col-12 text-center">
+        <div class="text-caption text-grey-4">© 2023 Harlow's Bar</div>
+      </div>
+    </div>
   </q-footer>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'AppFooter',
   setup () {
-    const { t } = useI18n()
-    return { t }
+    return {}
   }
 })
 </script>
