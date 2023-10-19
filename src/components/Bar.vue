b764e2dffb3cee907b97ed8cf328efe8a43a98cd
Cody <cody@bitaccel.com>
2023-10-19 16:45:22 +0200

    fix responsiveness of header bar

--- src/components/Bar.vue
+++ src/components/Bar.vue
@@ -5,14 +5,14 @@
       flat
       transparent
       class="text-weight-regular"
-      size="md"
+      :size="$q.screen.lt.sm ? 'sm' : 'md'"
       href="https://maps.app.goo.gl/y8zXASHuxx2pLbyf9"
       target="_blank"
     >
       C. Lepant 4, València
+      <span :class="[ $q.screen.lt.sm ? 'q-px-sm' : 'q-px-md' ]">{{ t('daysOpen') }}</span>
+      20:00 – 01:30
     </q-btn>
-    <div class="text-uppercase" style="font-size: 14px;">{{ t('daysOpen') }}</div>
-    <div style="font-size: 14px;">20:00 – 01:30</div>
     <q-space/>
   </q-bar>
 </template>
@@ -20,12 +20,14 @@
 <script>
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
   name: 'Bar',
   setup () {
+    const $q = useQuasar()
     const { t } = useI18n()
-    return { t }
+    return { $q, t }
   }
 })
 </script>
