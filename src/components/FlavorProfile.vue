bd3ee974af0642eea7785028c519b213ca3c7bd5
Cody <cody@bitaccel.com>
2023-12-02 12:47:26 +0100

    wip: flavor profile wheel

---
+++ src/components/FlavorProfile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flavor-chart">
+    <MartiniGlass />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import MartiniGlass from 'src/components/MartiniGlass.vue'
+
+export default defineComponent({
+  name: 'FlavorProfile',
+  components: { MartiniGlass },
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss">
+  .flavor-chart {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 300px;
+    width: 300px;
+  }
+</style>
---
+++ src/components/MartiniGlass.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="martini-glass"></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MartiniGlass',
+  setup () {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss">
+.martini-glass {
+  position: relative;
+  border-left: 50px solid transparent;
+  border-right: 50px solid transparent;
+  border-top: 60px solid #c3eef4;
+
+  &::before, &::after {
+    position: absolute;
+    content: '';
+  }
+
+  &::before {
+    z-index: -2;
+    width: 2px;
+    height: 20px;
+    top: -1px;
+    left: -1px;
+    background: #c3eef4;
+  }
+
+  // &::after {
+  //   width: 40px;
+  //   height: 2px;
+  //   top: 51px;
+  //   left: -19px;
+  //   background: #c3eef4;
+  // }
+}
+</style>
