b4c6aa4f36b6af0090c130f918bc45368b027492
Cody <cody@bitaccel.com>
2023-12-04 14:56:46 +0100

    color props

--- src/components/MartiniGlass.vue
+++ src/components/MartiniGlass.vue
@@ -12,6 +12,18 @@ import { computed, defineComponent } from 'vue'
 export default defineComponent({
   name: 'MartiniGlass',
   props: {
+    glassColor: {
+      type: String,
+      default: '#c3eef4'
+    },
+    liquidColor: {
+      type: String,
+      default: '#60D4CB'
+    },
+    liquidLevel: {
+      type: Number,
+      default: 1
+    },
     rotate: {
       type: Number,
       default: 0
@@ -25,10 +37,14 @@ export default defineComponent({
     const rotation = computed(() => {
       return props.rotate + 'deg'
     })
+    const liquidSideBorder = computed(() => {
+      return Math.ceil((parseInt(sideBorder) - 8) * (props.liquidLevel / 5)) + 'px'
+    })
+    const liquidTopBorder = computed(() => {
+      return Math.ceil((parseInt(topBorder) - 8) * (props.liquidLevel / 5)) + 'px'
+    })
     const sideBorder = Math.ceil(props.size * (5 / 6)) + 'px'
     const topBorder = props.size + 'px'
-    const liquidSideBorder = Math.ceil((parseInt(sideBorder) - 8) * (5 / 5)) + 'px'
-    const liquidTopBorder = Math.ceil((parseInt(topBorder) - 8) * (5 / 5)) + 'px'
     const stemHeight = Math.ceil(props.size / 4) + 'px'
     const stemWidth = Math.ceil(parseInt(stemHeight) / 5) + 'px'
     const circleSize = Math.ceil(parseInt(stemHeight) / 4) + 'px'
@@ -58,7 +74,7 @@ export default defineComponent({
     position: relative;
     border-left: v-bind(sideBorder) solid transparent;
     border-right: v-bind(sideBorder) solid transparent;
-    border-top: v-bind(topBorder) solid #c3eef4;
+    border-top: v-bind(topBorder) solid v-bind(glassColor);
 
     &::before, &::after {
       content: '';
@@ -75,7 +91,7 @@ export default defineComponent({
       height: v-bind(stemHeight);
       top: calc(v-bind(stemWidth) / -2);
       left: calc(v-bind(stemWidth) / -2);
-      background: #c3eef4;
+      background: v-bind(glassColor);
     }
 
     &::after { // central circle
@@ -83,7 +99,7 @@ export default defineComponent({
       height: v-bind(circleSize);
       top: v-bind(circleSpacer);
       left: calc(v-bind(circleSize) / -2 );
-      background: #c3eef4;
+      background: v-bind(glassColor);
       border-radius: 50%;
     }
 
@@ -92,7 +108,7 @@ export default defineComponent({
       right: calc(v-bind(liquidSideBorder) * -1);
       border-left: v-bind(liquidSideBorder) solid transparent;
       border-right: v-bind(liquidSideBorder) solid transparent;
-      border-top: v-bind(liquidTopBorder) solid #60D4CB;
+      border-top: v-bind(liquidTopBorder) solid v-bind(liquidColor);
     }
   }
 }
