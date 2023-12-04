9d7e343ce948fece172d1ed0567720ba9511f3c7
Cody <cody@bitaccel.com>
2023-12-04 17:26:30 +0100

    add olives

--- src/components/flavor/glass/Boozy.vue
+++ src/components/flavor/glass/Boozy.vue
@@ -1,6 +1,10 @@
 <template>
   <BaseGlass v-bind="props">
-    <div class="olive"></div>
+    <div class="pick">
+      <div class="olive"></div>
+      <div class="olive"></div>
+      <div class="olive"></div>
+    </div>
   </BaseGlass>
 </template>
 
@@ -42,28 +46,54 @@ export default defineComponent({
   },
   setup (props) {
     const pickSize = Math.ceil(props.size * (5 / 6)) + 'px'
+    const oliveSize = Math.ceil(props.size * (1 / 5)) + 'px'
     return {
       props,
-      pickSize
+      pickSize,
+      oliveSize
     }
   }
 })
 </script>
 
 <style lang="scss">
-.olive {
+.pick {
   position: absolute;
   background: rgb(138, 98, 50);
   height: calc(v-bind(pickSize) * 1.25);
   width: 2px;
   transform: skewX(140deg);
-  // top: calc(v-bind(pickSize) * -1 / 6);
   bottom: 10px;
   left: calc(v-bind(pickSize) * 1.5);
 
-  &::before , &::after {
+  &::before, &::after {
     position: absolute;
     content: '';
+  }
+  .olive {
+    position: absolute;
+    border-radius: 50%;
+    width: v-bind(oliveSize);
+    height: v-bind(oliveSize);
+    transform: skewX(35deg);
+    background: rgb(101, 112, 97);
+    // left: calc(v-bind(oliveSize) * 1.5);
+    left: calc(v-bind(oliveSize) * -1 / 2);
+
+    &:nth-child(1) {
+      bottom: calc(v-bind(oliveSize) * .3);
+    }
+    &:nth-child(2) {
+      bottom: calc(v-bind(oliveSize) * 1.15);
+    }
+    &:nth-child(3) {
+      bottom: calc(v-bind(oliveSize) * 2);
+    }
+
+    &::before, &::after{
+      position: absolute;
+      content: '';
+    }
   }
 }
 </style>
