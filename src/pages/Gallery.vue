79bb809d166e259f46a5fa7c5e00937a7634ee2a
Cody Manshack <cody.manshack@gmail.com>
2024-03-12 17:30:20 +0100

    add gallery

---
+++ src/pages/Gallery.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page>
+    <section class="q-pb-xl">
+    </section>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, reactive } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+  name: 'Gallery',
+  setup () {
+    const images = [
+      'cocktails/sidecar/0.25x.png',
+      'cocktails/blackmojito/0.33x.png',
+      'cocktails/hemingwayspecial/0.25x.png'
+    ]
+    const { t } = useI18n({})
+    return { images, t }
+  },
+  methods: {}
+})
+</script>
+
+<style lang="scss">
+</style>
