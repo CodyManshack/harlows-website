76f59559d5e64fa9572091b997f7531466af4174
Cody Manshack <cody.manshack@gmail.com>
2025-04-03 11:39:46 +0200

    update menu

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -110,7 +110,7 @@ export default defineComponent({
     const router = useRouter()
     const $q = useQuasar()
     const { t, locale } = useI18n({ useScope: 'global' })
-    const menuPath = computed(() => { return `${locale.value.toUpperCase()}_Harlow's Menu_11.12.pdf` })
+    const menuPath = computed(() => { return `${locale.value.toUpperCase()}_Harlow's Menu_02.04.pdf` })
     return {
       drawer: ref(false),
       router,
