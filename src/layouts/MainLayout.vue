21d417d98ee22cf9600644fe7eb751a3c2cc7c28
Cody Manshack <cody.manshack@gmail.com>
2025-10-11 12:54:12 +0200

    rm footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,16 @@
 <template>
-  <q-layout container style="height: 100svh;" view="hHh lpr fff">
+  <q-layout container style="height: 100svh" view="hHh lpr fff">
     <AppBar />
 
     <q-page-container>
       <router-view />
     </q-page-container>
 
-    <AppFooter />
+    <!-- <AppFooter /> -->
   </q-layout>
 </template>
 
 <script setup>
-import AppBar from '../components/AppBar.vue'
-import AppFooter from '../components/AppFooter.vue'
+import AppBar from "../components/AppBar.vue";
+import AppFooter from "../components/AppFooter.vue";
 </script>
