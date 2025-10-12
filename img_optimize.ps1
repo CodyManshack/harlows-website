a423369387b02dad8b87475219fc2a5378daf68f
Cody Manshack <cody.manshack@gmail.com>
2025-10-12 15:48:49 +0200

    rm example shit

--- img_optimize.ps1
+++ img_optimize.ps1
@@ -38,7 +38,3 @@ foreach ($sz in $sizes) {
 if ($tmpSrc -ne $Source -and (Test-Path $tmpSrc)) {
     Remove-Item $tmpSrc
 }
-
-Write-Host "`nExample srcset HTML:`n"
-$srcset = "<img src=\"$Base/$Base-800.jpg\" srcset=\"$Base/$Base-400.jpg 400w, $Base/$Base-800.jpg 800w, $Base/$Base-1200.jpg 1200w, $Base/$Base-2000.jpg 2000w\" sizes=\"(max-width: 600px) 400px, (max-width: 900px) 800px, (max-width: 1400px) 1200px, 2000px\" alt=\"Drink name and description\" loading=\"lazy\" width=\"800\" height=\"800\" />"
-Write-Output $srcset
