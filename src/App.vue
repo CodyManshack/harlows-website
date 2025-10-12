939a2e272c101e03161ee78fe1b60444ce6a7854
Cody Manshack <cody.manshack@gmail.com>
2025-10-12 16:15:25 +0200

    compress background in same way - move to public and adjust css

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,21 @@
 </template>
 
 <script setup>
-import { useMeta, useQuasar } from 'quasar'
+import { useMeta, useQuasar } from "quasar";
 
-const $q = useQuasar()
-$q.dark.set(true)
+const $q = useQuasar();
+$q.dark.set(true);
 useMeta({
   meta: {
-    keywords: { name: 'keywords', content: 'bar, cocktail bar, cocktail, great cocktails, coctelería, bars, valencia, bares, copas, speakeasy' },
-    equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' }
-  }
-})
+    keywords: {
+      name: "keywords",
+      content:
+        "bar, cocktail bar, cocktail, great cocktails, coctelería, bars, valencia, bares, copas, speakeasy",
+    },
+    equiv: {
+      "http-equiv": "Content-Type",
+      content: "text/html; charset=UTF-8",
+    },
+  },
+});
 </script>
-
-<style lang="scss">
-@media (max-width: $breakpoint-sm-max) {
-  body.body--dark {
-    background: linear-gradient(180deg, rgba(0,0,0,0.4), rgba(0,0,0,0.6)), url('./assets/location/bar/0.25x.png') no-repeat fixed;
-    background-position: 57% 0%;
-    background-size: cover;
-    background-attachment: fixed;
-  }
-}
-@media (max-width: $breakpoint-lg-max) {
-  body.body--dark {
-    background: linear-gradient(180deg, rgba(0,0,0,0.4), rgba(0,0,0,0.6)), url('./assets/location/bar/0.33x.png') no-repeat fixed;
-    background-position: 57% 0%;
-    background-size: cover;
-    background-attachment: fixed;
-  }
-}
-@media (min-width: $breakpoint-xl-min) {
-  body.body--dark {
-    background: linear-gradient(180deg, rgba(0,0,0,0.4), rgba(0,0,0,0.6)), url('./assets/location/bar/0.5x.png') no-repeat fixed;
-    background-position: 57% 45%;
-    background-size: cover;
-    background-attachment: fixed;
-  }
-}
-</style>
