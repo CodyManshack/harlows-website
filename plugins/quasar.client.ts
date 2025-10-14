8476d32b6961105c41364dec1598a9e6bbfad21e
Cody Manshack <cody.manshack@gmail.com>
2025-10-14 19:48:56 +0200

    add icons

--- plugins/quasar.client.ts
+++ plugins/quasar.client.ts
@@ -1,9 +1,11 @@
 import { Quasar } from "quasar";
 import "quasar/src/css/index.sass";
 import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/mdi-v7/mdi-v7.css";
 import "@quasar/extras/ionicons-v4/ionicons-v4.css";
 import "@quasar/extras/roboto-font/roboto-font.css";
 
+// @ts-ignore - Nuxt auto-imports defineNuxtPlugin
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(Quasar, {
     plugins: {},
